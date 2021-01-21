0ef3e47b5b701b17357255e0b19055d61fb5bd3d
Renato Benage <renatobenage@gmail.com>
2021-01-21 18:48:09 -0300

    several

---
+++ exercises/bloco_8/script.js
@@ -0,0 +1,51 @@
+const array = [1, 2, 3];
+array.push(4)
+
+console.log(array)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const assert = require('assert');
+
+//     const singer = {
+//       name: 'Milton Nascimento',
+//       birthplace: 'Rio de Janeiro',
+//       city: 'Belo Horizonte',
+//       albuns: ['Minas', 'Geraes', 'Clube da Esquina', 'Cançao da America', 'Três pontas', 'Nascimento', 'Caçador de Mim']
+//     }
+
+//     const singer2 = {
+//       ame: 'Annita',
+//       birthplace: 'Rio de Janeiro',
+//       city: 'Rio de Janeiro',
+//       albuns: ['Xalbua 1', 'Xalbua 2']
+//     }
+
+//     const {name, birthplace, city, albuns} = singer;
+
+//     console.log(singer);
