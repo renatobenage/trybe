7e0913c53548dcd0cf8ad1b66eee8a516fb93f25
Renato Benage <renatobenage@gmail.com>
2021-01-28 14:46:23 -0300

    nada

--- exercises/bloco_8/script.js
+++ exercises/bloco_8/script.js
@@ -1,8 +1,73 @@
-// const name = 'Maria'
+const musicians = [
+  {name: 'Renato', instrument: 'bass', official: true, inTheBand: ''},
+  {name: 'João', instrument: 'drum', official: false, inTheBand: ''},
+  {name: 'Evandro', instrument: 'piano', official: true, inTheBand: ''},
+];
 
-function name(value) {
-  for (let index = 0; index < name.length; index++) {
-    console.log(value.length)
+const includeBand = xablau => {
+  if (xablau.official) {
+    xablau.inTheBand = 'Toca na Band'
+  } else {
+    xablau.inTheBand = 'Ainda não'
   }
 }
-console.log(name('Maria'))
+
+musicians.forEach(includeBand)
+
+console.log(musicians)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // const students = [
+  //   { name: 'Maria', grade: 70, approved: '' },
+  //   { name: 'José', grade: 56, approved: '' },
+  //   { name: 'Roberto', grade: 90, approved: '' },
+  //   { name: 'Ana', grade: 81, approved: '' }
+  // ];
+
+  // console.log('string aqui!', students)
+
+  // function verifyGrade (xablau){
+  //   if (xablau.grade >= 60) {
+  //     xablau.approved = 'Aprovado';
+  //   } else {
+  //     xablau.approved = 'Recuperação';
+  //   }
+  // }
+
+  // let xena;
+  // for (xena = 0; xena < students.length; xena += 1) {
+  //   verifyGrade(students[xena]);
+  // }
+
+  // console.log(students);
+  // => [
+  //     { name: 'Maria', grade: 70, approved: 'Aprovado' },
+  //     { name: 'José', grade: 56, approved: 'Recuperação' },
+  //     { name: 'Roberto', grade: 90, approved: 'Aprovado' },
+  //     { name: 'Ana', grade: 81, approved: 'Aprovado' }
+  //   ]
