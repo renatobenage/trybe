ffc7deb90f5374499f2e57e1fb68fdf07c090cea
Renato Benage <renatobenage@gmail.com>
2021-01-28 14:47:06 -0300

    nada

---
+++ exercises/bloco_9/script.js
@@ -0,0 +1,21 @@
+const arrayObject = [
+  {
+    name: 'Renato',
+  },
+  {
+    lastname: 'Benage',
+  },
+  {
+    city: 'Rio de Janeiro',
+  },
+];
+
+console.log(arrayObject);
+
+console.log(`${arrayObject}`);
+
+// const array = ['Renato', 'Benage', "Rio de Janeiro"];
+
+// console.log("normal" + arrayObject);
+
+// console.log(`${arrayObject}`);
