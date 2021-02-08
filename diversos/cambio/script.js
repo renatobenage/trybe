367ea1e3ef8fd67f7a8730deddb60e1f0aa2db14
Renato Benage <renatobenage@gmail.com>
2021-02-08 20:08:46 -0300

    tudao

--- diversos/cambio/script.js
+++ diversos/cambio/script.js
@@ -1,5 +1,11 @@
 const setupEvents = () => {
-
+  // setar os eventos
+  // Ter em mãos meus elementos
+  const searchButton = document.querySelector('#search-button');
+  searchButton.addEventListener('click', () => {
+    console.log('Button clicado')
+    // pesquisar na api usando o texto no input
+  });
 }
 
 window.onload = () => {
