d9428459c9688167f9ac310ba6c1b8e550c660ec
renatobenage <renatobenage@gmail.com>
2021-02-10 11:02:36 -0300

    nada

--- exercises/bloco_5/script.js
+++ exercises/bloco_5/script.js
@@ -1,32 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const ingredients = [
-    'Feijão',
-    'Arroz',
-    'Ovos',
-    'Carne seca',
-    'Toucinho',
+  'Feijão',
+  'Arroz',
+  'Ovos',
+  'Carne seca',
+  'Toucinho',
 ];
 
-const acessUl = document.querySelector('.ingredients-item');
+console.log(ingredients)
 
-for (let index = 0; index < ingredients.length; index += 1) {
-    const through = ingredients[index];
-    // console.log(through)
-    const createlI = document.createElement('li');
-    // console.log(createlI)
-    createlI.innerText = through;
-    createlI.className = 'ingredients-item-list'
-    // console.log(createlI)
-    acessUl.appendChild(createlI)
-}
-
-let ingredientListItems = document.querySelectorAll('.ingredients-item-list')
-// console.log(ingredientListItems)
-
-for (let index = 0; index < ingredientListItems.length; index += 1) {
-    let element = ingredientListItems[index]
-    // console.log(element)
-
-    if (element.innerHTML.includes('toucinho') {
-        acessUl.removeChild();
-    }
-}
