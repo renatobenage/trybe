68011bd63b3912e70896bbe6056a2e94d8258645
renatobenage <renatobenage@gmail.com>
2021-03-03 21:00:21 -0300

    nada

---
+++ exercises/bloco_11/11.1/testando-meu-computador/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Renato Benage</h1>
+        <p>Sou esforçado!</p>
+      </div>
+    )
+  }
+}
+
+export default Component;
