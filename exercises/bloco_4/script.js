d7ed316d54461e11005ed0dff490ebfbf765c9e9
renatobenage <renatobenage@gmail.com>
2021-03-11 23:20:49 -0300

    nada

--- exercises/bloco_4/script.js
+++ exercises/bloco_4/script.js
@@ -1,12 +1,10 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let impares = []
+for (let i = 1; i < array.length; i++) {
+  for (let j = 0; j < i; j++) {
+    if (array[i] < array[j]) {
+      let position = array[i];
 
-for(let index = 0; index < numbers.length; index ++) {
-  if (numbers[index] % 2 !== 0) {
-    impares.push(numbers[index])
-  }else {
-    impares.push('nenhum valor ímpar encontrado')
+      array[i] = array[j];
+      array[j] = position;
+    }
   }
 }
-
-console.log(impares)
