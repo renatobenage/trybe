607d82ed28c154e21c4737c4af4a95dfe0cb32b9
renatobenage <renatobenage@gmail.com>
2021-05-19 10:01:35 -0300

    dd

---
+++ exercises/bloco_7/7.1/script.js
@@ -0,0 +1,34 @@
+
+const printName = name => name;
+console.log(printName("Lucas"));
+
+
+
+
+
+
+
+
+
+// function objetoPessoa (nome, idade) {
+//   return {
+//     nome: nome,
+//     idade: idade,
+//   }
+// }
+// console.log(objetoPessoa('Renato', 35))
+
+// const objetoPessoaArrow = (nome, idade) => ({nome: nome, idade:idade,})
+// console.log(objetoPessoaArrow('Isaah', 21))
+
+
+
+
+
+// function contaPalavras (frase) {
+//   return frase.split(' ').length
+// }
+// console.log(contaPalavras('Fala tribo, beleza?'))
+
+// const contaPalavrasArrow = frase => frase.split(' ').length
+// console.log(contaPalavrasArrow('Fala tribo, beleza?'))
